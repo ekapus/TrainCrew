2e8e7fc06b95947834fdf6652c05221fdcad692d
Ed Kapuscinski <ed@kapuscinski.net>
2017-10-19 19:07:35 -0700

    Add config.js and loading it on all pages.

---
+++ js/config.js
@@ -0,0 +1,8 @@
+// What factor to (x:1) to use for fast clock calculations
+// Used when calculating how long things like pumping air or walking a train takes.
+var FAST_CLOCK_SPEED = 4; 
+
+// What is the maximum chance (.1 = 10% max) of a failure of an airbrake component.
+// Used when calculating the failure rate of a failure during airbrake tests.
+var MAX_AIRBRAKE_FAILURE_RATE = .1; 
+
