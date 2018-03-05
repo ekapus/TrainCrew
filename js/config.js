4628649eb3f763f67cbf11c6aeb6c572ccfabce3
Ed Kapuscinski <ed@kapuscinski.net>
2018-03-05 17:31:10 -0500

    Adding some more error settings, unlocking a switch, and converting sounds from wav to mp3

--- js/config.js
+++ js/config.js
@@ -5,3 +5,9 @@ var FAST_CLOCK_SPEED = 4;
 // What is the maximum chance (.1 = 10% max) of a failure of an airbrake component.
 // Used when calculating the failure rate of a failure during airbrake tests.
 var MAX_AIRBRAKE_FAILURE_RATE = .1; 
+
+
+// What is the maximum chance (.1 = 10% max) that handbrakes applied aren't enough.
+// Used when calculating the failure rate of a failure during handbrake tests.
+var MAX_HANDBRAKE_FAILURE_RATE = .1; 
+MAX_HANDBRAKE_FAILURE_RATE = 1; 
--- js/site.js
+++ js/site.js
@@ -30,6 +30,28 @@ function PlaySoundWithCountdown(soundFile, soundDuration, badgeClass) {
     $(badgeClass).text(soundDuration);
 }
 
+function PlaySoundWithCountdownAndFailure(soundFile, soundDuration, badgeClass, failureChance, statusElement, failureCauses) {
+    console.log("Starting PlaySoundWithCountdownAndFailure(" + soundFile + ", " + soundDuration + ", " +  badgeClass + ", " +  failureChance + ", " +  statusElement + ", " +  failureCauses);
+    $(badgeClass).addClass("badge-warning");
+    $(badgeClass).text(soundDuration);
+    $(badgeClass).show();
+    var audio = new Audio(soundFile);
+    $(badgeClass).text(soundDuration);
+    audio.play();
+    var failure = decideFailure(failureChance);
+    console.log("failure: " + failure)
+    if(failure){
+        var defectCode = Math.floor(Math.random()*failureCauses.length);
+        var defectType = failureCauses[defectCode];
+        PerformCountDownOnBadgeAndSetMessage(badgeClass,"badge-error",statusElement,defectType,);
+        
+    }
+    
+    $(badgeClass).text(soundDuration);
+    
+    return failure;
+
+}
 
 
 function PerformCountDownOnBadge(badgeClass) {
@@ -44,6 +66,26 @@ function PerformCountDownOnBadge(badgeClass) {
             $(badgeClass).removeClass("badge-warning");
             $(badgeClass).addClass("badge-success");
             PerformBadgeReset(badgeClass, 1, seconds);
+        } // final action
+    });
+    countDown.start();
+}
+
+function PerformCountDownOnBadgeAndSetMessage(badgeClass, badgeFinalClass, statusElement, statusHTML) {
+    var seconds = parseInt($(badgeClass).text());
+    console.log(seconds);
+    var countDown = new Countdown({
+        seconds: seconds,  // number of seconds to count down
+        onUpdateStatus: function (sec) {
+            $(badgeClass).text(sec);
+        }, // callback for each second
+        onCounterEnd: function () {
+            $(badgeClass).removeClass("badge-warning");
+            $(badgeClass).addClass(badgeFinalClass);
+            statusElement.html(defectType);
+            if(performBadgeReset){
+                PerformBadgeReset(badgeClass, 1, seconds);
+            }
         } // final action
     });
     countDown.start();
